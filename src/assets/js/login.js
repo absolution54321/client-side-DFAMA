6d7d7f5e1c2171c0d675a68a2a25d2c594688d93
absolution54321 <shrutarshis8@gmail.com>
2017-07-22 02:46:57 +0530

    mentorHome Complete

--- src/assets/js/login.js
+++ src/assets/js/login.js
@@ -1,41 +1,52 @@
 var app = angular.module("app");
 
-app.controller("login", function($scope, $location,$http) {
+app.controller("login", function($scope, $location, $http, $cookies) {
 
   $scope.data={};
   //console.log(data.user);
 
-
-  $scope.clicked = function(){
-       console.log($scope.data);
+  $scope.clicked = function()
+  {
+    console.log($scope.data);
     $scope.jsonObject = {
-                        "UserId":$scope.data.user,
-                        "UserPwd": $scope.data.pwd,
-                        "UserType":$scope.data.result
-                    };
+                          "UserId":$scope.data.user,
+                          "UserPwd": $scope.data.pwd,
+                          "UserType":$scope.data.result
+                         };
         
         var url = "http://localhost:3010/";
         var hpromise = $http.post(url, $scope.jsonObject);
-        hpromise.then(function(response) {
-              console.log(response);
+        
+        hpromise.then(function(response) 
+        {
+            console.log(response.data[0].mentorId);
             //routing according to login
               if(response.data.length>0)
               {
-                    if($scope.data.result==1){
-                         $location.path("/adminHome");
-                    }else if($scope.data.result==2){
-                         $location.path("/studentHome");
-                    }else if($scope.data.result==3){
-                         $location.path("/mentorHome");
+                  if($scope.data.result==1)
+                  {
+                    $location.path("/adminHome");
+                  }
+                  else if($scope.data.result==2)
+                  {
+                    $location.path("/studentHome");
+                  }
+                  else if($scope.data.result==3)
+                  {
+                    $location.path("/mentorHome");
+                    
+                    $cookies.put('mentorId', response.data[0].mentorId);
+                    $cookies.put('mentorUserName', response.data[0].mentorUsername);
+                  }
               }
-            }else
+              else
+              {
+                alert("Invalid Credentials");
+              }
+        }).catch(function(err) 
             {
-              alert("Invalid Credentials");
-            }
-        }).catch(function(err) {
-            console.log(err);
-        });
-
-          
+              console.log(err);
+            });        
  };  
+
 });
--- src/assets/js/mentorHome.js
+++ src/assets/js/mentorHome.js
@@ -1,8 +1,10 @@
 var app = angular.module("app");
 
 
-app.controller("mentorHome", function($scope, $window)
+app.controller("mentorHome", function($scope, $window, $cookies, $http)
 {
+    $scope.mentorData = {};
+
     $scope.goHome = function()
     {
         $window.location.href = "http://localhost:3000/#!/mentorHome";
@@ -18,5 +20,35 @@ app.controller("mentorHome", function($scope, $window)
         $window.location.href = "http://localhost:3000/#!/teamPerformance";
     }
     
+    $scope.mentorInit = function()
+    {
+        $scope.mentorData.id = $cookies.get('mentor');
+        console.log("Hi");   
+        
+        $scope.jsonObject = { "mentorId": $cookies.get('mentorId') }
+        
+        var url = "http://localhost:3010/mentor";
+        var hpromise = $http.post(url, $scope.jsonObject);
+        
+        hpromise.then(function(response) 
+        {
+            //routing according to login
+            console.log(response);
+            $scope.mentorData.userName = $cookies.get('mentorUserName');
+            $scope.mentorData.name = response.data[0].mentorName;
+            $scope.mentorData.yoe = response.data[0].yearOfExperience;
+            $scope.mentorData.contact = response.data[0].contactNo;
+            $scope.mentorData.company = response.data[0].company;
+            $scope.mentorData.aoe = response.data[0].areaOfExpertise;
+            $scope.mentorData.batch = response.data[0].batch;
+            $scope.mentorData.email = response.data[0].email;
+            $scope.mentorData.teamNo = response.data[0].teamId;
+
+        }).catch(function(err) 
+            {
+              console.log(err);
+            });        
+ };  
+
 
 });
