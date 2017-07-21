42324c7ba7f91e9d3f2d2446bf3d5e672e4386db
suchita0106 <suchita21.dabir@gmail.com>
2017-07-21 21:29:08 +0530

    login webservice invokation

--- src/assets/js/login.js
+++ src/assets/js/login.js
@@ -1,6 +1,6 @@
 var app = angular.module("app");
 
-app.controller("login", function($scope, $location) {
+app.controller("login", function($scope, $location,$http) {
 
   $scope.data={};
   //console.log(data.user);
@@ -8,15 +8,34 @@ app.controller("login", function($scope, $location) {
 
   $scope.clicked = function(){
        console.log($scope.data);
+    $scope.jsonObject = {
+                        "UserId":$scope.data.user,
+                        "UserPwd": $scope.data.pwd,
+                        "UserType":$scope.data.result
+                    };
+        
+        var url = "http://localhost:3010/";
+        var hpromise = $http.post(url, $scope.jsonObject);
+        hpromise.then(function(response) {
+              console.log(response);
+            //routing according to login
+              if(response.data.length>0)
+              {
+                    if($scope.data.result==1){
+                         $location.path("/adminHome");
+                    }else if($scope.data.result==2){
+                         $location.path("/studentHome");
+                    }else if($scope.data.result==3){
+                         $location.path("/mentorHome");
+              }
+            }else
+            {
+              alert("Invalid Credentials");
+            }
+        }).catch(function(err) {
+            console.log(err);
+        });
 
-       //routing according to login
-       if($scope.data.result==1){
-                $location.path("/adminHome");
-       }else if($scope.data.result==2){
-                $location.path("/studentHome");
-       }else if($scope.data.result==3){
-                 $location.path("/mentorHome");
-       }
- };
-
+          
+ };  
 });
