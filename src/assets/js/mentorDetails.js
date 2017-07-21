4aecb39ea8ec00e937462bf5a795f2121f262081
sonalidamute <sonalidamute@gmail.com>
2017-07-21 12:51:32 +0530

    added files from shubham for admin

--- src/assets/js/mentorDetails.js
+++ src/assets/js/mentorDetails.js
@@ -1,26 +1,26 @@
 var app = angular.module("app");
 
 
-app.controller("adminHome", function($scope,$location) {
+app.controller("mentorDetails", function($scope) {
 
-$scope.custom = true;
+    $scope.custom = true;
 $scope.custom1 = true;
 $scope.toggleMarks = function(){
     $scope.custom = $scope.custom === false ? true : false;
 }
 
-$scope.toggleData = function(){
+$scope.toggleData1 = function(){
     $scope.custom1 = $scope.custom1 === false ? true : false;
 }
 
 
  $scope.goToForum=function(){
-        $location.path("/mentorDetails");
+        $location.path("/student");
     };
 
     $scope.goToMentorDetails=function(){
         $location.path("/mentor");
     };
 
-    
+
 });
