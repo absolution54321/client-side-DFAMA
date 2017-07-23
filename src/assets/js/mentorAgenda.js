8bfe317df036fb9f9603d0207eca563fae977e40
absolution54321 <shrutarshis8@gmail.com>
2017-07-23 16:28:12 +0530

    agenda Done

--- src/assets/js/mentorAgenda.js
+++ src/assets/js/mentorAgenda.js
@@ -1,9 +1,10 @@
 var app = angular.module("app");
 
 
-app.controller("mentorAgenda", function($scope, $window,$compile)
+app.controller("mentorAgenda", function($scope, $window,$compile, $filter, $http, $cookies, $location)
 {
-    $scope.postsList =[];
+    $scope.postList =[];
+    $scope.posts = {};
 
     $scope.fetch = function()
     {
@@ -25,12 +26,58 @@ app.controller("mentorAgenda", function($scope, $window,$compile)
         $window.location.href = "http://localhost:3000/#!/teamPerformance";
     };
 
-    $scope.setAgenda = function(){
-        // console.log($scope.posts.agendaText);
-        // var newEle = angular.element('<div>$scope.posts.agendaText</div>');
-        // $compile(newEle)($scope);
-        
+    $scope.setAgenda = function()
+    {
+        //$scope.posts.agendaDate = new Date($scope.posts.agendaDate);
 
+        $scope.posts.agendaDate = $filter('date')($scope.posts.agendaDate, "dd-MM-yyyy"); // for conversion to string
+        $scope.postList.push({"agendaText":$scope.posts.agendaText, "agendaDate":$scope.posts.agendaDate});
+        
+        $scope.jsonObj = { "mentorId": $cookies.get('userId'),
+                            "agendaText":$scope.posts.agendaText,
+                            "agendaDate":$scope.posts.agendaDate
+                        };
+
+        var url = "http://localhost:3010/mentor/agenda";
+        var hpromise = $http.post(url, $scope.jsonObj);
+        
+        hpromise.then(function(response) 
+        {
+            console.log(response);
+            
+        }).catch(function(err) 
+            {
+              console.log(err);
+            });
+
+    };
+
+    $scope.agendaInit = function()
+    {
+        $scope.jsonObj = { "mentorId": $cookies.get('userId') };
+
+        var url = "http://localhost:3010/mentor/agendaInit";
+        var hpromise = $http.post(url, $scope.jsonObj);
+        
+        hpromise.then(function(response) 
+        {
+            console.log(response);
+
+            if(response.data.length > 0)
+                {
+                    for(var i=0; i< response.data.length; i++)
+                        {
+                            $scope.postList.push({"agendaText": response.data[i].agenda_Details,
+                                                   "agendaDate": response.data[i].scheduleTime
+                                                    });
+                        }
+                    
+                }
+            
+        }).catch(function(err) 
+            {
+              console.log(err);
+            });
     };
 
     $scope.performLogOut = function(){
