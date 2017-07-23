eb86540c10891d6e9997e8fcdb864cc3a05c9a08
absolution54321 <shrutarshis8@gmail.com>
2017-07-23 19:45:28 +0530

    teamPerformance indented

--- src/assets/js/mentorAgenda.js
+++ src/assets/js/mentorAgenda.js
@@ -1,86 +1,75 @@
 var app = angular.module("app");
 
 
-app.controller("mentorAgenda", function($scope, $window,$compile, $filter, $http, $cookies, $location)
-{
-    $scope.postList =[];
+app.controller("mentorAgenda", function ($scope, $window, $compile, $filter, $http, $cookies, $location) {
+    $scope.postList = [];
     $scope.posts = {};
 
-    $scope.fetch = function()
-    {
+    $scope.fetch = function () {
         $window.location.href = "https://www.google.co.in/";
     };
 
-    $scope.goHome = function()
-    {
+    $scope.goHome = function () {
         $window.location.href = "http://localhost:3000/#!/mentorHome";
     };
 
-    $scope.go = function()
-    {
+    $scope.go = function () {
         $window.location.href = "http://localhost:3000/#!/mentorAgenda";
     };
 
-    $scope.goTeam = function()
-    {
+    $scope.goTeam = function () {
         $window.location.href = "http://localhost:3000/#!/teamPerformance";
     };
 
-    $scope.setAgenda = function()
-    {
+    $scope.setAgenda = function () {
         //$scope.posts.agendaDate = new Date($scope.posts.agendaDate);
 
         $scope.posts.agendaDate = $filter('date')($scope.posts.agendaDate, "dd-MM-yyyy"); // for conversion to string
-        $scope.postList.push({"agendaText":$scope.posts.agendaText, "agendaDate":$scope.posts.agendaDate});
-        
-        $scope.jsonObj = { "mentorId": $cookies.get('userId'),
-                            "agendaText":$scope.posts.agendaText,
-                            "agendaDate":$scope.posts.agendaDate
-                        };
+        $scope.postList.push({ "agendaText": $scope.posts.agendaText, "agendaDate": $scope.posts.agendaDate });
+
+        $scope.jsonObj = {
+            "mentorId": $cookies.get('userId'),
+            "agendaText": $scope.posts.agendaText,
+            "agendaDate": $scope.posts.agendaDate
+        };
 
         var url = "http://localhost:3002/mentor/agenda";
         var hpromise = $http.post(url, $scope.jsonObj);
-        
-        hpromise.then(function(response) 
-        {
+
+        hpromise.then(function (response) {
             console.log(response);
-            
-        }).catch(function(err) 
-            {
-              console.log(err);
-            });
+
+        }).catch(function (err) {
+            console.log(err);
+        });
 
     };
 
-    $scope.agendaInit = function()
-    {
+    $scope.agendaInit = function () {
         $scope.jsonObj = { "mentorId": $cookies.get('userId') };
 
         var url = "http://localhost:3002/mentor/agendaInit";
         var hpromise = $http.post(url, $scope.jsonObj);
-        
-        hpromise.then(function(response) 
-        {
+
+        hpromise.then(function (response) {
             console.log(response);
 
-            if(response.data.length > 0)
-                {
-                    for(var i=0; i< response.data.length; i++)
-                        {
-                            $scope.postList.push({"agendaText": response.data[i].agenda_Details,
-                                                   "agendaDate": response.data[i].scheduleTime
-                                                    });
-                        }
-                    
+            if (response.data.length > 0) {
+                for (var i = 0; i < response.data.length; i++) {
+                    $scope.postList.push({
+                        "agendaText": response.data[i].agenda_Details,
+                        "agendaDate": response.data[i].scheduleTime
+                    });
                 }
-            
-        }).catch(function(err) 
-            {
-              console.log(err);
-            });
+
+            }
+
+        }).catch(function (err) {
+            console.log(err);
+        });
     };
 
-    $scope.performLogOut = function(){
+    $scope.performLogOut = function () {
         $cookies.remove("userId");
         $cookies.remove("mentorUserName");
         $location.path("/");
--- src/assets/js/teamPerformance.js
+++ src/assets/js/teamPerformance.js
@@ -1,59 +1,51 @@
 var app = angular.module("app");
 
-app.controller("teamPerformance", function($scope, $window, $http, $cookies, $location)
-{
+app.controller("teamPerformance", function ($scope, $window, $http, $cookies, $location) {
     $scope.teamList = [];
     $scope.teamDetails = {};
 
-    $scope.goHome = function()
-    {
+    $scope.goHome = function () {
         $window.location.href = "http://localhost:3000/#!/mentorHome";
     };
 
-    $scope.go = function()
-    {
+    $scope.go = function () {
         $window.location.href = "http://localhost:3000/#!/mentorAgenda";
     };
 
-    $scope.goTeam = function()
-    {
+    $scope.goTeam = function () {
         $window.location.href = "http://localhost:3000/#!/teamPerformance";
     };
 
-    $scope.teamInit = function()
-    {
+    $scope.teamInit = function () {
         $scope.jsonObj = { "mentorId": $cookies.get('userId') };
 
         //console.log($scope.jsonObj.mentorId);
 
         var url = "http://localhost:3002/mentor/team";
-        var hpromise =  $http.post(url, $scope.jsonObj);
+        var hpromise = $http.post(url, $scope.jsonObj);
 
-        hpromise.then(function(response)
-        {
+        hpromise.then(function (response) {
             console.log(response);
-            if(response.data.length > 0)
-                {
-                    for(var i=0; i< response.data.length; i++)
-                        {
-                            $scope.teamList.push({ "rank": response.data[i].rank,
-                                                    "name": response.data[i].name,
-                                                    "yearofpassing": response.data[i].yearofpassing,
-                                                    "qualification": response.data[i].qualification
-                                                });
-                        }
+            if (response.data.length > 0) {
+                for (var i = 0; i < response.data.length; i++) {
+                    $scope.teamList.push({
+                        "rank": response.data[i].rank,
+                        "name": response.data[i].name,
+                        "yearofpassing": response.data[i].yearofpassing,
+                        "qualification": response.data[i].qualification
+                    });
                 }
+            }
 
             $scope.teamDetails.groupName = response.data[0].groupName;
             $scope.teamDetails.teamId = response.data[0].teamId;
 
-        }).catch(function(err)
-            {
-                console.log(err);
-            });
+        }).catch(function (err) {
+            console.log(err);
+        });
     };
 
-    $scope.performLogOut = function(){
+    $scope.performLogOut = function () {
         $cookies.remove("userId");
         $cookies.remove("mentorUserName");
         $location.path("/");
